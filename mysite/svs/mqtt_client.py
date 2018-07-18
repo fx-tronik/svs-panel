353717ab438ad7a0dd6cc7e1e06c3247a865dade
Michał Pieprzycki <michal.pieprzycki@fx-tronik.pl>
2018-07-18 11:18:25 +0200

    rename mqtt -> mqtt_client, create second instance for second worker

--- mysite/svs/mqtt_client.py
+++ mysite/svs/mqtt_client.py
@@ -2,10 +2,14 @@ from .models import Infrasctructure
 import paho.mqtt.client as mqtt
 import json
 
-SUB_TOPICS = ("ws-arm-imp", "ws-arm-cr", "ws-arm-bas", "SVS_callback1")
+SUB_TOPICS_CV = ("ws-arm-imp", "ws-arm-cr", "ws-arm-bas")
+SUB_TOPICS_ES = ("SVS_callback",)
 RECONNECT_DELAY_SECS = 2
 
 
+# Custom client class to catch exceptions and dump them into ws-debug topic.
+# Without it, on excepton the client thread hangs and does not recover untill
+# restart
 class CustomMqttClient(mqtt.Client):
 
     def _handle_on_message(self, message):
@@ -17,27 +21,30 @@ class CustomMqttClient(mqtt.Client):
 
 
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
+def on_connect_CV(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    for topic in SUB_TOPICS:
+    for topic in SUB_TOPICS_CV:
+        print(topic)
         client.subscribe(topic, qos=1)
 
 
-def on_connect2(client, userdata, flags, rc):
+def on_connect_ES(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    for topic in SUB_TOPICS:
-        client.subscribe("SVS_callback1", qos=1)
+    for topic in SUB_TOPICS_ES:
+        print(topic)
+        client.subscribe(topic, qos=1)
 
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    if msg.topic == "SVS_callback1":
+    if msg.topic == "SVS_callback":
+        print(msg.payload)
         j = json.loads(str(msg.payload.decode("utf-8", "ignore")))
         for group in j:
             print(group)
@@ -45,7 +52,8 @@ def on_message(client, userdata, msg):
                 infrastructure, created = Infrasctructure.objects.get_or_create(type=group, no=i+1)
                 infrastructure.value = value
                 infrastructure.save()
-
+    elif msg.topic == "ws-arm-cr":
+        print(msg.payload)
 
 def on_publish(mosq, obj, mid):
     print("mid: " + str(mid))
@@ -68,20 +76,18 @@ def on_disconnect(client, userdata, rc):
 
 
 client = CustomMqttClient()
-client2 = CustomMqttClient()
-client.on_connect = on_connect
+client.on_connect = on_connect_CV
 client.on_message = on_message
 # client.on_publish = on_publish
 client.on_subscribe = on_subscribe
 client.on_disconnect = on_disconnect
 
-client2.on_connect = on_connect2
+
+client2 = CustomMqttClient()
+client2.on_connect = on_connect_ES
 client2.on_message = on_message
 # client2.on_publish = on_publish
 client2.on_subscribe = on_subscribe
 client2.on_disconnect = on_disconnect
 
-
-# client.username_pw_set(username, password)
-client.connect('192.168.0.200', 1885, 60)
-client2.connect('192.168.0.200', 1885, 60)
+#client.connect('192.168.0.200', 1885, 60)
