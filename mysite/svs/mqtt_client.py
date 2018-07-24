9acae353bd88671902b74da6d72881393742c09d
Michał Pieprzycki <michal.pieprzycki@fx-tronik.pl>
2018-07-24 15:44:48 +0200

    introduce proper logging support, WIP

--- mysite/svs/mqtt_client.py
+++ mysite/svs/mqtt_client.py
@@ -1,5 +1,7 @@
 from .models import Infrasctructure
 import paho.mqtt.client as mqtt
+from .logger import logger
+
 import json
 
 SUB_TOPICS_CV = ("ws-arm-imp", "ws-arm-cr", "ws-arm-bas")
@@ -10,6 +12,8 @@ RECONNECT_DELAY_SECS = 2
 # Custom client class to catch exceptions and dump them into ws-debug topic.
 # Without it, on excepton the client thread hangs and does not recover untill
 # restart
+
+
 class CustomMqttClient(mqtt.Client):
 
     def _handle_on_message(self, message):
@@ -17,27 +21,28 @@ class CustomMqttClient(mqtt.Client):
             super(CustomMqttClient, self)._handle_on_message(message)
         except Exception as e:
             error = {"exception": str(e.__class__.__name__), "message": str(e)}
+            logger.critical(json.dumps(error))
             self.publish("ws-debug", json.dumps(error), qos=1)
 
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect_CV(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    logger.info("Connected with result code " + str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     for topic in SUB_TOPICS_CV:
-        print(topic)
+        logger.info(topic)
         client.subscribe(topic, qos=1)
 
 
 def on_connect_ES(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
+    logger.info("Connected with result code " + str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     for topic in SUB_TOPICS_ES:
-        print(topic)
+        logger.info(topic)
         client.subscribe(topic, qos=1)
 
 
@@ -56,23 +61,23 @@ def on_message(client, userdata, msg):
         print(msg.payload)
 
 def on_publish(mosq, obj, mid):
-    print("mid: " + str(mid))
-    print('wyslalem')
+    logger.info('Message published')
+
 
 def on_subscribe(mosq, obj, mid, granted_qos):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+    logger.info("Subscribed: " + str(mid) + " " + str(granted_qos))
 
 
 def on_log(mosq, obj, level, string):
-    print(string)
+    logger.info(string)
 
 
 def on_disconnect(client, userdata, rc):
     client.loop_stop(force=False)
     if rc != 0:
-        print("Unexpected disconnection: rc:" + str(rc))
+        logger.critical("Unexpected disconnection: rc:" + str(rc))
     else:
-        print("Disconnected: rc:" + str(rc))
+        logger.info("Disconnected: rc:" + str(rc))
 
 
 client = CustomMqttClient()
