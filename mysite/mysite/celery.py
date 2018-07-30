9b2bdc4ce31d54cf383d709339daa41931380034
Michał Pieprzycki <michal.pieprzycki@fx-tronik.pl>
2018-07-30 09:11:59 +0200

    move mqtt init back to celery

--- mysite/mysite/celery.py
+++ mysite/mysite/celery.py
@@ -9,12 +9,21 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 app = Celery('mysite')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
+
 app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
+@celeryd_init.connect()
+def testujemy(conf=None, **kwargs):
 
-#@celeryd_init.connect()#sender='worker12@example.com')
-#def testujemy(conf=None, **kwargs):
+    from svs import mqtt_client as mqtt
+    mqtt.client.loop_stop()
+    mqtt.client.connect('192.168.0.200', 1885, 60)
+    mqtt.client.loop_start()
+
+    mqtt.client2.loop_stop()
+    mqtt.client2.connect('192.168.0.200', 1885, 60)
+    mqtt.client2.loop_start()
